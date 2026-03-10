d008a8c50440496327cb820206c488c73467269d
dundun <dundun@howoldareyou.com>
2026-03-10 10:06:33 +0800

    fix: 恢复百度统计内联脚本方式

--- src/components/BaiduAnalytics.jsx
+++ src/components/BaiduAnalytics.jsx
@@ -10,7 +10,15 @@ function BaiduAnalytics() {
   }
 
   return (
-    <Script id="baidu-tongji" strategy="afterInteractive" src={`https://hm.baidu.com/hm.js?${analyticsId}`} />
+    <Script id="baidu-tongji" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?${analyticsId}";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+    ` }} />
   )
 }
 
